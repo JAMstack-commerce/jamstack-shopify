b3b2c8661c6d4942035d2950d1e0cffc042061c4
Rihards Paskausks <pundurs@glhf.lv>
2019-11-07 11:18:05 +0200

    Replace debut theme CSS with buefy. (#4)

---
+++ plugins/buefy.ts
@@ -0,0 +1,8 @@
+import { Vue } from 'nuxt-property-decorator';
+
+// TODO: Possibly, replace with individually
+// imported components. See:
+// https://github.com/buefy/buefy#2-import-and-use-buefy
+import Buefy from 'buefy';
+
+Vue.use(Buefy);
